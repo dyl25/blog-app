5fe34221b8113d007e9dd8c9c93f0f0d8b353512
dyl25 <dylan-vst@hotmail.com>
2023-01-24 20:30:05 +0100

    add first page

---
+++ pages/articles/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <p>test</p>
+    </div>
+</template>
+
+<script setup>
+    const { data:articles } = await useFetch('http://localhost:8000/api/v1/articles')
+</script>
