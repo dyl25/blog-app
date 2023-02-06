3bd0eea0a5b50f146da3f57768e721f39c9af1b8
dyl25 <dylan-vst@hotmail.com>
2023-02-06 17:22:59 +0100

    add new pages

---
+++ pages/articles/create.vue
@@ -0,0 +1,34 @@
+<script setup>
+//first create virtual host with laragon
+  const {data: articles} = await useFetch('http://blog-api.test/api/v1/articles')
+
+  let isLoading = ref(false)
+
+  function onSubmit() {
+
+  }
+
+</script>
+
+<template>
+  <div>
+    <h2>Article creation</h2>
+    <div>
+      <form @submit.prevent="onSubmit">
+        <fieldset>
+          <div>
+            <label for="title">Title</label>
+            <input type="text" id="title" name="title">
+          </div>
+
+          <div>
+            <label for="content">Content</label>
+            <textarea id="content" name="content"></textarea>
+          </div>
+
+          <button type="submit">Create</button>
+        </fieldset>
+      </form>
+    </div>
+  </div>
+</template>
